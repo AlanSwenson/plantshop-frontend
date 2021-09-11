c0a4bcfb5ffda56a1ac7dd479af5b7a34148ca69
AlanSwenson <alan@alanswenson.dev>
2021-09-10 23:09:42 -0500

    small changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gradient-to-r from-purple-800 to-blue-500">
       <header className="App-header">
-        <button class="bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 rounded-md text-lg px-5 py-1">
+        <button class="shadow-md bg-green-400 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 rounded-md text-lg px-5 py-1">
           Mint
         </button>
 
